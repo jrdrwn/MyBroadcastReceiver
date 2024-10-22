8cc60391660b845bf905203e4dc1649b9ef7c288
Jordi Irawan <64052045+jrdrwn@users.noreply.github.com>
2024-10-22 07:00:10 +0700

    Latihan BroadcastReceiver dari Custom Event

--- app/src/main/java/com/wan/mybroadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/wan/mybroadcastreceiver/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.wan.mybroadcastreceiver
 
 import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.wan.mybroadcastreceiver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var downloadReceiver: BroadcastReceiver
     private var binding: ActivityMainBinding? = null
 
     var requestPermissionLauncher =
@@ -22,6 +31,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
         }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,6 +39,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding?.root)
 
         binding?.btnPermission?.setOnClickListener(this)
+        binding?.btnDownload?.setOnClickListener(this)
+
+        downloadReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                Toast.makeText(context, "Download selesai", Toast.LENGTH_SHORT).show()
+            }
+        }
+        val downloadIntentFilter = IntentFilter(ACTION_DOWNLOAD_STATUS)
+        registerReceiver(
+            downloadReceiver,
+            downloadIntentFilter,
+            RECEIVER_NOT_EXPORTED
+        )
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -40,13 +64,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.btn_permission -> requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+            R.id.btn_download -> {
+                Handler(Looper.getMainLooper()).postDelayed(
+                    {
+                        val notifyFinishIntent = Intent().setAction(ACTION_DOWNLOAD_STATUS)
+                        // custom action should add this line in >= android 14
+                        notifyFinishIntent.setPackage(this.packageName)
+                        sendBroadcast(notifyFinishIntent)
+                    },
+                    3000
+                )
+            }
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterReceiver(downloadReceiver)
         binding = null
     }
 
+    companion object {
+        const val ACTION_DOWNLOAD_STATUS = "download_status"
+    }
 
 }
